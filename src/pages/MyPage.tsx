aa339e8afc6f2690905593a0d5baa98872da7ec3
lovvp <67538804+lovvp@users.noreply.github.com>
2021-03-04 02:25:47 +0900

    feature/page/mypage

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import MyPageStore from "../components/MyPageStore";
+import MyPageProfile from "../components/MyPageProfile";
+import MyPageLikes from "../components/MyPageLikes";
+import MyPageRequest from "../components/MyPageRequest";
+import "../styles/MyPage.css";
 
 function MyPage() {
 	return (
-		<div>
-			{/* 밑에 h1 삭제하고 컴포넌트 입력하기 */}
-			<h1>마이 페이지 입니다.</h1>
-			{/* todo */}
+		<div id="mypage">
+			<Header />
+			<section className="mypage__container">
+				<MyPageProfile />
+				<MyPageRequest />
+				<MyPageStore />
+				<MyPageLikes />
+			</section>
+			<Footer />
 		</div>
 	);
 }
