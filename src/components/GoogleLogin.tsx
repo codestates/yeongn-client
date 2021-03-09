07575788e2bab0ddea6c05c65c0b48e52f21b6da
lovvp <67538804+lovvp@users.noreply.github.com>
2021-03-09 23:05:00 +0900

    update login

--- src/components/GoogleLogin.tsx
+++ src/components/GoogleLogin.tsx
@@ -13,7 +13,7 @@ interface ILoginUser extends RouteComponentProps {
 }
 function GoogleLogin({ loginHandler, history, location }: ILoginUser) {
 	const GOOGLE_LOGIN_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile&access_type=offline&include_granted_scopes=true&state=state_parameter_passthrough_value&
-redirect_uri=https://www.yeongn.com/login&response_type=code&client_id=
+redirect_uri=http://localhost:3000/login&response_type=code&client_id=
 604944373689-q294luegtuje1qpkiq0q3jrfqd8ps6qp.apps.googleusercontent.com`;
 
 	const googleLoginHandler = () => {
@@ -22,17 +22,18 @@ redirect_uri=https://www.yeongn.com/login&response_type=code&client_id=
 	};
 
 	const getAuth = (authorizationCode: string) => {
-		const url = "/api/user/google";
+		const url = "https://www.yeongn.com/api/user/google";
 		console.log("jebal");
 		axios
 			.post(url, { authorizationCode }, { withCredentials: true })
 			.then((res) => {
-				loginHandler({
-					userId: res.data.nickname,
-					userEmail: res.data.email,
-					authenticated: true,
-				});
-				history.push("/");
+				console.log(res.data);
+				// loginHandler({
+				// 	userId: res.data.nickname,
+				// 	userEmail: res.data.email,
+				// 	authenticated: true,
+				// });
+				// history.push("/");
 			})
 			.catch(() => {
 				console.log("ssibal");
@@ -41,8 +42,8 @@ redirect_uri=https://www.yeongn.com/login&response_type=code&client_id=
 
 	useEffect(() => {
 		const url = new URL(window.location.href);
-		console.log(location);
-		console.log(history);
+		// console.log(location);
+		// console.log(history);
 		const authorizationCode = url.searchParams.get("code");
 		const googleCheck = window.location.href.indexOf("google");
 		if (authorizationCode && googleCheck !== -1) {
--- src/components/KakaoLogin.tsx
+++ src/components/KakaoLogin.tsx
@@ -19,15 +19,14 @@ interface ILoginUser extends RouteComponentProps {
 }
 function KakaoLogin() {
 	const history = useHistory();
-	const KAKAO_LOGIN_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=6uSvluf8fHGhNvp6U3j2&redirect_uri=http://localhost:3000/login`;
-
-	const naverLoginHandler = () => {
+	const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=b862dc01a142ef533360f21219d2247b&redirect_uri=http://localhost:3000/login`;
+	const kakaoLoginHandler = () => {
 		window.location.assign(KAKAO_LOGIN_URL);
-		console.log("navernavernavernavernavernaver");
+		console.log("kakaokakaokakaokakaokakaokakaokakao");
 	};
 
 	const getAuth = (authorizationCode: any) => {
-		const url = "https://yeongn.com/api/user/naver";
+		const url = "https://yeongn.com/api/user/kakao";
 		axios.post(url, { authorizationCode }).then((res) => {
 			console.log(res.data);
 		});
@@ -36,15 +35,16 @@ function KakaoLogin() {
 	useEffect(() => {
 		const url = new URL(window.location.href);
 		const authorizationCode = url.searchParams.get("code");
-		const kakaoCheck = window.location.href.indexOf("google");
-		if (authorizationCode && kakaoCheck === -1) {
+		const kakaoCheck = window.location.href.indexOf("login");
+		const googleCheck = window.location.href.indexOf("google");
+		if (authorizationCode && kakaoCheck !== -1 && googleCheck === -1) {
 			console.log("Kakao", authorizationCode);
 			// getAuth(authorizationCode);
 		}
 	});
 
 	return (
-		<div className="loginContainer">
+		<div className="loginContainer" onClick={kakaoLoginHandler}>
 			<img src={KakaoBtn} alt="kakaobutton" className="loginBtn" />
 		</div>
 	);
